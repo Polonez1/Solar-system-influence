74411e3188958593c1ce999e1dd94549f304deb9
Polonez1 <darjus.vasiukevic@gmail.com>
2024-01-13 18:23:31 +0200

    .

--- formulas.py
+++ formulas.py
@@ -1,3 +1,6 @@
+import math
+
+
 class Formula:
     G: float = 6.6743 * 10**-11  # Gravitation constant
     Em: float = 6 * 10**24  # Earth mass (kg)
@@ -7,3 +10,10 @@ class Formula:
         "Newton's law of universal gravitation"
         F = (m1 * m2 * self.G) / r**2
         return F
+
+    def distance_from_grawitylaw(self, m1, m2, F):
+        # r = √( (G · m1 · m2)/F )
+        G = self.G
+        r = math.sqrt((G * m1 * m2) / F)
+
+        return r
